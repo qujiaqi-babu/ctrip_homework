fb452dbb4ad88131035e6335c5b551d2bae50588
Babu <1058509662@qq.com>
2024-03-27 22:38:04 +0800

    底部导航栏测试

--- wander-log-app/src/HomePage/index.js
+++ wander-log-app/src/HomePage/index.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Button, Text, View } from "react-native";
 
-export default function MyLogScreen({ navigation }) {
+export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>My Log Screen</Text>
+      <Text>Home Screen</Text>
       <Button
         title="游记详情"
         onPress={() => navigation.navigate("LogDetail")}
       />
-      <Button title="登录" onPress={() => navigation.navigate("Login")} />
     </View>
   );
 }
--- wander-log-app/src/LoginPage/index.js
+++ wander-log-app/src/LoginPage/index.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function LoginScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Login Screen</Text>
     </View>
   );
 }

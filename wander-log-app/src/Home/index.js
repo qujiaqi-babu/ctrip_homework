7bf91ab151b1bc79a9a4fa94d676b9135815d47c
Babu <1058509662@qq.com>
2024-03-27 19:49:24 +0800

    底部导航栏

---
+++ wander-log-app/src/Home/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
